26958621c89f84a3aae5452139e53c239d834421
Ekoko Clesh <36325257+Ekoko-Clesh@users.noreply.github.com>
2024-08-02 03:45:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -9,7 +9,6 @@ def home():
         return "Hello, this is a GET request!"
     elif request.method == 'POST':
         return "Hello, this is a POST request!"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
